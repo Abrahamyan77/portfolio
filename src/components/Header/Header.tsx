6a9f3bccc964b757ad6404109b82b975389f0d67
GriogrAbrahamayanIngo <grigor.abrahamyan@ingoarmenia.am>
2026-01-02 20:43:32 +0400

    add tailwind and create files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+import React from 'react';
+import './Header.css';
+
+const Header: FC = () => {
+  return <header>This is the header</header>;
+};
+export default Header;
